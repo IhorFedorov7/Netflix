e3f5234f0fa27818bb7e4aeee995d4b57e41f3fb
Ihor <ihor.fedorov.life@gmail.com>
2019-05-25 23:39:02 +0300

    Верстка страницы Netflix с элементами js

---
+++ js/code.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+// Select tab content item 
+function selectItem(e) {
+    // Remove all show and border classes 
+    removeBorder();
+    removeShow();
+    // Add border to current tab 
+    this.classList.add('tab-border');
+    // Grab content item from DOM 
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class 
+    tabContentItem.classList.add('show');
+}
+// Remove bottom borders from all tab items 
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+// Remove show class from all content items 
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+// Listen for tab click 
+tabItems.forEach(item => item.addEventListener('click', selectItem));
